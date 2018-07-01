3c6a220be96e7e2f0b58dfb732806a7fc4b5a5c8
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2018-07-01 18:38:37 +0300

    Improve getting application scriptes

--- framework.js
+++ framework.js
@@ -1,42 +1,48 @@
 'use strict';
 
-const http = require('http');
-const https = require('https');
 const tools = require('common-toolkit');
 
 const scripts = require('./lib/scripts.js');
 const server = require('./lib/server.js');
 const applications = require('./lib/applications.js');
 
-const APP_DIR = './application';
-
 const config = {
-  port: 3000,
-  host: 'localhost'
+  application: {
+    directory: './application'
+  },
+
+  modules: ['http', 'https'],
+
+  server: {
+    port: 3000,
+    host: 'localhost'
+  }
 };
 
 const api = {
   tools,
-  http,
-  https
 };
 
-const lib = [
-  'api.application.js',
-  'api.requests.js'
-]
-  .map(path => APP_DIR + '/lib/' + path);
+config.modules
+  .forEach(
+    module => Object.assign(
+      api, { [module]: require(module) }
+    )
+  );
+
+const applicationScripts = applications
+  .getApplicationScripts(config.application);
 
 const sandbox = scripts.createSandbox();
 const runner = scripts.prepareRunner(api, sandbox);
 
 scripts.runApplicationScripts(
-  [lib],
+  applicationScripts,
   runner
 );
 
 applications.createApp(
-  APP_DIR,
+  config.application.directory,
   api,
   sandbox,
   (err, app) => {
@@ -45,7 +51,7 @@ applications.createApp(
       process.exit(1);
     }
 
-    server.boundServer(app, config);
+    server.boundServer(app, config.server);
     console.log('Server bound');
   }
 );
