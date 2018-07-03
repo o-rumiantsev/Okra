402eb03e37a8e0bac3d5155663f508ffbe3c8454
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2018-07-03 13:20:47 +0300

    Improve example

--- application/api/get.js
+++ application/api/get.js
@@ -1,7 +1,6 @@
 (req, res) => {
   api.writeHello(res);
-  const info = `Application: ${!!application};\n` +
-    `Api: ${!!api};`;
+  const info = `Api: ${!!api};`;
   res.write(info);
   api.console.log(api.db);
   res.end();
